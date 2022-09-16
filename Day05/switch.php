fcebdeafd34b484dd6aabcaa53730e3677875c23
ashmanmalhotra <ashmanmalhotra@gmail.com>
2022-09-16 12:54:23 +0530

    switch
    switch

---
+++ Day05/switch.php
@@ -0,0 +1,16 @@
+<?php
+$id = 1;
+switch($id){
+    case 1:
+        echo "ID is 1";
+        break;
+    case 2:
+        echo "ID is 2";
+        break;
+    case 3:
+        echo "ID is 3";
+        break;
+    default:
+        echo "ID is not 1, 2 or 3";
+        break;
+}
