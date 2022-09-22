2b8e3251001d1cffa4a01d1507531b59065a8095
Ashman Malhotra <ashmanmalhotra@gmail.com>
2022-09-22 15:31:55 +0530

    up
    up

--- project/admin/user_add.php
+++ project/admin/user_add.php
@@ -1,3 +1,28 @@
+<?php include_once("includes/dbconnection.php") ?>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === "POST"){
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        $role = $_POST['role'];
+        $active = $_POST['userstate'];
+
+        if(!empty($firstname) || !empty($lastname) || !empty($email) || !empty($password)){
+            $query = "INSERT INTO users (firstname, lastname, email, password, role, active) VALUES ";
+            $query .= "('$firstname', '$lastname', '$email', '$password', '$role', '$active')";
+    
+            $result = mysqli_query($conn, $query);
+            $addition_operation = true;
+        }
+    }
+
+?>
+
+
+
+
 <?php include_once("includes/header.php") ?>
 
 <!-- Page Wrapper -->
@@ -21,9 +46,20 @@
         <div class="container-fluid">
 
             <!-- Page Heading -->
-            <h1 class="h3 mb-4 text-gray-800">Add a User</h1>
+            <h1 class="h3 mb-4 text-gray-800">Create a User</h1>
 
-            <form>
+            <?php
+                if(isset($addition_operation)){
+                    if($result){
+                        echo "<div class='alert alert-success'>User Added Successfully</div>";
+                    }
+                    else{
+                        echo "<div class='alert alert-danger'>Error Adding User</div>";
+                    }
+                }
+            ?>
+
+            <form action="user_add.php" method="POST">
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="email">Email</label>
@@ -48,17 +84,21 @@
                     <div class="form-group col-md-6">
                         <label for="role">Role</label>
                         <select class="custom-select mr-sm-2" id="role" name="role">
-                            <option selected>Choose...</option>
-                            <option value="0">User</option>
+                            <option disabled>Choose...</option>
+                            <option value="0" selected>User</option>
                             <option value="1">Admin</option>
                         </select>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="lastname">Last Name</label>
-                        <input type="text" class="form-control" id="lastname" name="lastname">
+                        <label for="userstate">User State</label>
+                        <select class="custom-select mr-sm-2" id="userstate" name="userstate">
+                            <option disabled>Choose...</option>
+                            <option value="1" selected>Active</option>
+                            <option value="0">Inactive</option>
+                        </select>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <button type="submit" class="btn btn-primary">Create User</button>
             </form>
 
 
