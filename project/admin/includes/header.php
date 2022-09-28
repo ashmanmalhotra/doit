20fd8d8649b5da75a50575e9fb5dd13b22da1362
Ashman Malhotra <ashmanmalhotra@gmail.com>
2022-09-28 11:19:32 +0530

    admin
    admin access control

--- project/admin/includes/header.php
+++ project/admin/includes/header.php
@@ -1,3 +1,10 @@
+<?php include_once('dbconnection.php') ?>
+<?php
+    if (!isset($_SESSION['userid'])) {
+        header("Location: /doitc/index.php");
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
